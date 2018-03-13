55d64bdcbc5d8bbaca518362c8b44ef0aae73375
Sim Tze Jan <tzejan@gmail.com>
2018-03-13 18:24:25 +0800

    Added solution for DELETE expand-url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const encode = require("./demo/encode");
 const decode = require("./demo/decode");
 
-function getNextId(){
+function getNextId() {
   return "" + (existingURLs.length + 1);
 }
 
@@ -23,10 +23,10 @@ app.post("/shorten-url", (req, res) => {
   let receivedURL = req.body.url;
   let urlHash = encode(receivedURL, existingURLs);
   let existingEntry = existingURLs.filter(data => data.hash === urlHash);
-  if (existingEntry.length === 0){
-    existingURLs.push({id: getNextId(), url: receivedURL, hash: urlHash})
+  if (existingEntry.length === 0) {
+    existingURLs.push({ id: getNextId(), url: receivedURL, hash: urlHash });
   }
-  let returnObj = {hash: urlHash}
+  let returnObj = { hash: urlHash };
   res.send(returnObj);
 });
 
@@ -34,17 +34,34 @@ app.post("/expand-url", (req, res) => {
   let receivedHash = req.body.hash;
   try {
     let storedURL = decode(receivedHash, existingURLs);
-    let result = {url: storedURL};
+    let result = { url: storedURL };
     res.send(result);
   } catch (error) {
-    let errorMsg = {"message": "There is no long URL registered for hash value 'MTAwMDA='"};
+    let errorMsg = {
+      message: `There is no long URL registered for hash value 'MTAwMDA='`
+    };
     res.status(404).send(errorMsg);
   }
-})
+});
 
-app.delete("/urls/:hash", (req, res) => {
+app.delete("/expand-url/:hash", (req, res) => {
   let hashToDelete = req.params.hash;
-})
+  try {
+    let storedURL = decode(hashToDelete, existingURLs);
+    existingURLs = existingURLs.filter(
+      element => element.hash !== hashToDelete
+    );
+    let message = {
+      message: `URL with hash value '${hashToDelete}' deleted successfully`
+    };
+    res.send(message);
+  } catch (error) {
+    let errorMsg = {
+      message: `URL with hash value '${hashToDelete}' does not exist`
+    };
+    res.status(404).send(errorMsg);
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
